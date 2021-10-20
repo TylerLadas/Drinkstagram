a8b5f6a0422a70cb41d13436b403bc5f8ce684fe
Yussef Rafat <yussefrafat5908@gmail.com>
2021-10-20 19:11:36 -0400

    test

---
+++ client/src/pages/SearchDrinks.js
@@ -0,0 +1,175 @@
+import React, { useState, useEffect } from 'react';
+import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+
+import Auth from '../utils/auth';
+import { searchCocktails } from '../utils/API';
+import { saveDrinkIds, getSavedDrinkIds } from '../utils/localStorage';
+
+import { useMutation } from '@apollo/client';
+import { SAVE_DRINK } from '../utils/mutations'
+
+const SearchDrinks = () => {
+  // create state for holding returned cocktail api data
+  const [searchedDrinks, setSearchedDrinks] = useState([]);
+  // create state for holding our search field data
+  const [searchInput, setSearchInput] = useState('');
+
+  // create state to hold saved drinkId values
+  const [savedDrinkIds, setSavedDrinkIds] = useState(getSavedDrinkIds());
+
+  // define saveDrink with mutation
+  const [saveDrink, { error }] = useMutation(SAVE_DRINK);
+
+  // set up useEffect hook to save `savedDrinkIds` list to localStorage on component unmount
+  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
+  useEffect(() => {
+    return () => saveDrinkIds(savedDrinkIds);
+  });
+
+  // create method to search for drinks and set state on form submit
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!searchInput) {
+      return false;
+    }
+
+    try {
+      const response = await searchCocktails(searchInput);
+
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
+
+      const { drinks } = await response.json();
+
+      const drinkData = drinks.map((drink) => ({
+        
+        drinkId: drink.idDrink,
+        title: drink.strDrink,
+        // ingredients: drink.strMeasure1 + " " + drink.strIngredient1 + ", " + 
+        //              drink.strMeasure2 + " " + drink.strIngredient2 + ", " + 
+        //              drink.strMeasure3 + " " + drink.strIngredient3 + ", " +
+        //              drink.strMeasure4 + " " + drink.strIngredient4 + ", " +
+        //              drink.strMeasure5 + " " + drink.strIngredient5 + ", " +
+        //              drink.strMeasure6 + " " + drink.strIngredient6 + ", " +
+        //              drink.strMeasure7 + " " + drink.strIngredient7 + ", " +
+        //              drink.strMeasure8 + " " + drink.strIngredient8 + ", " +
+        //              drink.strMeasure9 + " " + drink.strIngredient9 + ", " +
+        //              drink.strMeasure10 + " " + drink.strIngredient10,
+        ingredients: 
+                     drink.strMeasure1 + " " + drink.strIngredient1 + ", " +
+                     drink.strMeasure2 + " " + drink.strIngredient2 + ", " + 
+                     drink.strMeasure3 + " " + drink.strIngredient3 + ", " +
+                     drink.strMeasure4 + " " + drink.strIngredient4 + ", " +
+                     drink.strMeasure5 + " " + drink.strIngredient5 + ", " +
+                     drink.strMeasure6 + " " + drink.strIngredient6 + ", " +
+                     drink.strMeasure7 + " " + drink.strIngredient7 + ", " +
+                     drink.strMeasure8 + " " + drink.strIngredient8 + ", " +
+                     drink.strMeasure9 + " " + drink.strIngredient9 + ", " +
+                     drink.strMeasure10 + " " + drink.strIngredient10 || '',
+        
+        instructions: drink.strInstructions,
+        image: drink.strDrinkThumb || '',
+      }));
+
+      setSearchedDrinks(drinkData);
+      setSearchInput('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  
+  // create function to handle saving a drink to our database
+  const handleSaveDrink = async (drinkId) => {
+    // find the drink in `searchedDrinks` state by the matching id
+    const drinkToSave = searchedDrinks.find((drink) => drink.drinkId === drinkId);
+
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const { data } = await saveDrink({ 
+        variables: {input: drinkToSave}
+      });
+
+      if (error) {
+        throw new Error('something went wrong!');
+      }
+
+      // if drink successfully saves to user's account, save drink id to state
+      setSavedDrinkIds([...savedDrinkIds, drinkToSave.drinkId]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <>
+      <Jumbotron fluid className='text-light bg-dark'>
+        <Container>
+          <h1>Search for Drinks!</h1>
+          <Form onSubmit={handleFormSubmit}>
+            <Form.Row>
+              <Col xs={12} md={8}>
+                <Form.Control
+                  name='searchInput'
+                  value={searchInput}
+                  onChange={(e) => setSearchInput(e.target.value)}
+                  type='text'
+                  size='lg'
+                  placeholder='Search for a drink'
+                />
+              </Col>
+              <Col xs={12} md={4}>
+                <Button type='submit' variant='success' size='lg'>
+                  Submit Search
+                </Button>
+              </Col>
+            </Form.Row>
+          </Form>
+        </Container>
+      </Jumbotron>
+
+      <Container>
+        <h2>
+          {searchedDrinks.length
+            ? `Viewing ${searchedDrinks.length} results:`
+            : 'Search for a drink to begin'}
+        </h2>
+        <CardColumns>
+          {searchedDrinks.map((drink) => {
+            return (
+              <Card key={drink.drinkId} border='dark'>
+                {drink.image ? (
+                  <Card.Img src={drink.image} alt={`The cover for ${drink.title}`} variant='top' />
+                ) : null}
+                <Card.Body>
+                  <Card.Title>{drink.title}</Card.Title>
+                  <p className='small'>Ingredients: {drink.ingredients}</p>
+                  <Card.Text>Instructions: {drink.instructions}</Card.Text>
+                  {Auth.loggedIn() && (
+                    <Button
+                      disabled={savedDrinkIds?.some((savedDrinkId) => savedDrinkId === drink.drinkId)}
+                      className='btn-block btn-info'
+                      onClick={() => handleSaveDrink(drink.drinkId)}>
+                      {savedDrinkIds?.some((savedDrinkId) => savedDrinkId === drink.drinkId)
+                        ? 'This drink has already been saved!'
+                        : 'Save this Drink!'}
+                    </Button>
+                  )}
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
+      </Container>
+    </>
+  );
+};
+
+export default SearchDrinks;
--- server/models/User.js
+++ server/models/User.js
@@ -1,35 +1,39 @@
-const mongoose = require('mongoose');
-
-const { Schema } = mongoose;
+const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
-const Order = require('./Order');
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5
-  },
-  orders: [Order.schema]
-});
+// import schema from Drink.js
+const drinkSchema = require('./Drink');
 
-// set up pre-save middleware to create password
-userSchema.pre('save', async function(next) {
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    // set savedDrinks to be an array of data that adheres to the drinkSchema
+    savedDrinks: [drinkSchema],
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// hash user password
+userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
@@ -38,11 +42,16 @@ userSchema.pre('save', async function(next) {
   next();
 });
 
-// compare the incoming password with the hashed password
-userSchema.methods.isCorrectPassword = async function(password) {
-  return await bcrypt.compare(password, this.password);
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
 };
 
-const User = mongoose.model('User', userSchema);
+// when we query a user, we'll also get another field called `drinkCount` with the number of saved drinks we have
+userSchema.virtual('drinkCount').get(function () {
+  return this.savedDrinks.length;
+});
+
+const User = model('User', userSchema);
 
 module.exports = User;

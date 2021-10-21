a25f2875ba3e297760a0be1f24c9a5ac11ccfa3a
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-10-21 19:50:34 -0400

    manifest errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+const webpack = require('webpack');
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+
 const path = require('path');
 
 const config = {
@@ -31,6 +35,23 @@ const config = {
           }
         ]
     },
+    plugins: [
+        new WebpackPwaManifest({
+            name: "Drinkstagram",
+            short_name: "Drinks",
+            description: "An app that allows you to search for different drink recipes.",
+            start_url: "../App.js",
+            background_color: "#01579b",
+            theme_color: "#ffffff",
+            fingerprints: false,
+            inject: false,
+            icons: [{
+              src: path.resolve("assets/img/icons/icon-512x512.png"),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join("assets", "icons")
+            }]
+          })
+    ],
     loaders: [
         {
           test: /\.(graphql|gql)$/,

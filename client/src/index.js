a946ec3cee4fc247d0f5cf6b9e1355077b1f3bbc
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-10-22 11:00:32 -0400

    pwa service worker error

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 
 ReactDOM.render(
@@ -13,3 +15,7 @@ ReactDOM.render(
 );
 
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();

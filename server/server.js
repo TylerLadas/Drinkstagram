a68ad07aa43dff169fa9380edd458ef326a9146e
TylerLadas <tylerladas@hotmail.com>
2021-10-25 18:55:46 -0400

    add path to server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
